e7d6fa47ba9946c8903e7c197b81a39c27ed9dbb
RaulPeSilva <adraulpesilva@gmail.com>
2020-09-19 13:02:40 -0300

    add image and move button to shared

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../pages/login';
 
 const Routes = () => {
-  const logado = false;
+  const logado = true;
   return (
     <BrowserRouter>
       <Switch>
         {logado ? (
           <>
             <Route exact path="/" component={() => <h1>logado</h1>} />
-            <Route path="/teste" component={() => <h1>testes</h1>} />
+            <Route path="/room/:id" component={() => <h1>testes</h1>} />
           </>
         ) : (
           <Route exact path="/" component={Login} />
